de68a63e67d0647b498a0e1c706e01b218362ba3
陈俊豪 <347517726@qq.com>
2022-05-18 14:06:45 +0800

    陈俊豪:页面数据修改

--- web/src/core/config.js
+++ web/src/core/config.js
@@ -5,7 +5,7 @@
 const config = {
   appName: '河南天盛',
   appNamee: '陈俊豪',
-  appLogo: 'https://www.gin-vue-admin.com/img/logo.png',
+  appLogo: 'https://top-chen.com/api/v3/file/source/7032/logo.png?sign=RVlI69MH7t1_JIhskFoTrS9ejS-pw9jQB8Ik_QYDRrU%3D%3A0',
   showViteLogo: true
 }
 
@@ -14,7 +14,7 @@ export const viteLogo = (env) => {
     const chalk = require('chalk')
     console.log(
       chalk.green(
-        `> 欢迎使用Gin-Vue-Admin，开源地址：https://github.com/flipped-aurora/gin-vue-admin`
+        `> 欢迎使用tiansheng-admin-system，开源地址：https://github.com/Top-Chen0/tiansheng-admin-system`
       )
     )
     console.log(
@@ -24,12 +24,12 @@ export const viteLogo = (env) => {
     )
     console.log(
       chalk.green(
-        `> 加群方式:微信：shouzi_1994 QQ群：622360840`
+        `> 联系我方式:微信：347517726`
       )
     )
     console.log(
       chalk.green(
-        `> GVA讨论社区：https://support.qq.com/products/371961`
+        `> https://github.com/Top-Chen0/tiansheng-admin-system`
       )
     )
     console.log(
@@ -49,7 +49,7 @@ export const viteLogo = (env) => {
     )
     console.log(
       chalk.green(
-        `> 如果项目让您获得了收益，希望您能请团队喝杯可乐:https://www.github.com/flipped-aurora/gin-vue-admin/server.com/docs/coffee`
+        `> 如果项目让您获得了收益，希望您能请团队喝杯可乐:微信联系：347517726`
       )
     )
     console.log('\n')
